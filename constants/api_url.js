32be1532b82fb283766bb7764c304c1d76c3d126
omer kilincoglu <speak-ashy-parka@duck.com>
2025-07-22 02:58:12 +0300

    Vercel baglantisi eklelem + icon degistirme

--- constants/api_url.js
+++ constants/api_url.js
@@ -1,3 +1,3 @@
 //export const api_url = `http://10.7.84.251:3000`;
 //export const api_url = `http://192.168.111.83:3000`;
-export const api_url = `http://192.168.1.70:3000`;
+export const api_url = `https://bitirme-backend-proje.vercel.app`;
